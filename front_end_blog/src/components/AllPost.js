a890b30e1c3d520d2c9b0e16732cd04e93a1f3b5
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-02-24 22:04:49 +0200

    comments added

--- front_end_blog/src/components/AllPost.js
+++ front_end_blog/src/components/AllPost.js
@@ -8,7 +8,7 @@ function AllPost({post, readMore,setDisplay}){
                     <div className="col-lg-8 col-md-10 mx-auto">
                         {post.map((item) => (
                             <div className="post-preview" key={item.id}>
-                                <h2 className="post-title">{item.title}</h2>
+                                <h2 className="post-title">{item.title}<span className="font-weight-light"> ({item.comments.length})</span></h2>
                                 <h4 className="post-subtitle font-weight-light" id="postText">{item.body}</h4>
                                 <button className='btn btn-primary rounded float-right' onClick={()=>readMore(item.id)}>Read more</button>
                                 <p className="post-meta">Category: <strong className="text-dark">{item.category.category}</strong>. Posted by
--- front_end_blog/src/components/OnePost.js
+++ front_end_blog/src/components/OnePost.js
@@ -8,7 +8,16 @@ function OnePost({chosen, goBack}) {
             <p>Category: <strong className="text-dark">{chosen.category.category}</strong> / Posted
                 by <strong>{chosen.user.name}.</strong></p>
             <p>{chosen.body}</p>
-            <button className='btn btn-primary rounded float-right' onClick={goBack}>Go Back</button>
+            <button className='btn btn-primary rounded' onClick={goBack}>Go Back</button>
+            <hr/>
+            <div className="col-md-12">
+                {chosen.comments.length!== 0 ?<h3>Comments</h3>:<h5>No comments added yet</h5>}
+                {chosen.comments.map((comment) =>{
+                    let dateFormat = comment.created_at.split("T");
+                        return (
+                            <p key={comment.id}>{dateFormat[0]}: {comment.body}</p>
+                        )})}
+            </div>
         </div>
     )
 }
